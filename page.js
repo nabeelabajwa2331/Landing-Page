cd7789f04f784d998e5a23b649d1ee161b4f4cb5
nabeelabajwa2331 <ibrahim.bajwaaaa@gmail.com>
2025-11-20 14:21:56 +0500

    Initial project upload

---
+++ page.js
@@ -0,0 +1,5 @@
+ const mobileBtn = document.getElementById('mobileBtn');
+    const mobileMenu = document.getElementById('mobileMenu');
+    mobileBtn?.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
