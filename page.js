984af4621ee7e4b24f99e07a062a09705d10950c
nabeelabajwa2331 <ibrahim.bajwaaaa@gmail.com>
2025-11-24 13:01:48 +0500

    fix: correct files

--- page.js
+++ page.js
@@ -1,5 +1,5 @@
- const mobileBtn = document.getElementById('mobileBtn');
-    const mobileMenu = document.getElementById('mobileMenu');
-    mobileBtn?.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
+const mobileBtn = document.getElementById('mobileBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+mobileBtn?.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
